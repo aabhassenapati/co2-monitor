11ada3b1c938e1a462c98d13b9d08d75fd594382
Aabhas Senapati <aabhas2211@gmail.com>
2022-06-17 13:18:44 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,16 +1,19 @@
 #include <SD.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <RTCZero.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "Adafruit_CCS811.h"
+#include <Arduino.h>   // required before wiring_private.h
+#include "wiring_private.h" // pinPeripheral() function
 #define PCF8563address 0x51
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 // Declaration for SSD1306 display connected using software SPI (default case):
 #define OLED_MOSI   9
-#define OLED_CLK   10
-#define OLED_DC    11
+#define OLED_CLK   5
+#define OLED_DC    6
 #define OLED_CS    12
 #define OLED_RESET 13
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI,
@@ -18,10 +21,29 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI,
 byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
 String days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 Adafruit_CCS811 ccs;
+RTCZero rtc;
 const int chipSelect = 4;
+const byte MHZ19_CMD_READ_CO2[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
+const byte MHZ19_CMD_CALIBRATE_ZERO[9] = {0xFF,0x01,0x87,0x00,0x00,0x00,0x00,0x00,0x78}; 
+Uart Serial2 (&sercom1, 11, 10, SERCOM_RX_PAD_0, UART_TX_PAD_2);
+int ppm = 0;
+int ppmLast = 0;
+void SERCOM1_Handler()
+{
+  Serial2.IrqHandler();
+}
 void setup() {
   Serial.begin(9600);
-
+    Serial2.begin(9600);
+     pinPeripheral(10, PIO_SERCOM);
+  pinPeripheral(11, PIO_SERCOM);
+    pinMode(13, OUTPUT);
+  digitalWrite( 13, HIGH );
+  delay(1000);
+  digitalWrite( 13, LOW );
+  delay(500);
+  readCO2(1);
+  delay(1000);
   if (!display.begin(SSD1306_SWITCHCAPVCC)) {
     Serial.println(F("SSD1306 allocation failed"));
   }
@@ -45,10 +67,11 @@ Serial.print("DATE: ");
 Serial.print("TIME: ");
   Serial.print("\t");
      Serial.println();
+
 }
 void loop() {
   File dataFile = SD.open("co2_logs.txt", FILE_WRITE);
-  readPCF8563();
+ readPCF8563();
   display.clearDisplay();
   display.setCursor(0, 0);            // Start at top-left corner
   display.setTextSize(1);             // Draw 2X-scale text
@@ -57,10 +80,10 @@ void loop() {
   ccs.readData();
   float cco2 = ccs.geteCO2();
   display.println(cco2);
-  float mco2 = analogRead(A0);
-  mco2 = ((6.4995 * mco2) - 590.53); // format; y=mx+c
+  ppm = readCO2(0);
+   // format; y=mx+c
   display.print("CO2@M: ");
-  display.println(mco2);
+  display.println(ppm);
   display.print("DATE: ");
   display.print(dayOfMonth, DEC);
   display.print("/");
@@ -87,9 +110,9 @@ void loop() {
   Serial.print(cco2);
     Serial.print("\t");
  // Serial.print("CO2@M: ");
-  Serial.print(mco2);
+  Serial.print(ppm);
     Serial.print("\t");
- // Serial.print("DATE: ");
+// // Serial.print("DATE: ");
   Serial.print(dayOfMonth, DEC);
   Serial.print("/");
   Serial.print(month, DEC);
@@ -118,7 +141,7 @@ void loop() {
     dataFile.print("CO2@C: ");
     dataFile.println(cco2);
     dataFile.print("CO2@M: ");
-    dataFile.println(mco2);
+    dataFile.println(ppm);
     dataFile.print("DATE: ");
     dataFile.print(dayOfMonth, DEC);
     dataFile.print("/");
@@ -166,4 +189,43 @@ void readPCF8563()
   dayOfWeek  = bcdToDec(Wire.read() & B00000111);
   month      = bcdToDec(Wire.read() & B00011111);  // remove century bit, 1999 is over
   year       = bcdToDec(Wire.read());
+}
+int readCO2( int calibrate )
+{
+  // command to ask for data
+  char response[9]; // for answer
+
+  if( calibrate == 1)
+  {
+    Serial2.write(MHZ19_CMD_CALIBRATE_ZERO, 9); //request PPM CO2 calibration
+    Serial.println("Calibrating!");
+  }
+  else
+  {
+    Serial2.write(MHZ19_CMD_READ_CO2, 9); //request PPM CO2
+  }
+  //Serial2.write(MHZ19_CMD_READ_CO2, 9);
+  Serial2.readBytes(response, 9);
+  if (response[0] != 0xFF)
+  {
+    //Serial.println("Wrong starting byte from co2 sensor!");
+    return 0;
+  }
+
+  if (response[1] != 0x86)
+  {
+    Serial.println("MH redetected");
+     Serial2.readBytes(response, 9);
+    Serial2.write(MHZ19_CMD_READ_CO2, 9);
+     }
+
+  // Return value:
+  // FF=startbyte, 86=command, 02=high byte, 60=low byte, 00=4xnull bytes, 79=checksum
+  // byte cmd[9] = {0xFF, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xD1};
+  // Gas concentration: (high byte*256)+low byte
+
+  int responseHigh = (int) response[2];
+  int responseLow = (int) response[3];
+  int ppm = (256 * responseHigh) + responseLow;
+  return ppm;
 }
